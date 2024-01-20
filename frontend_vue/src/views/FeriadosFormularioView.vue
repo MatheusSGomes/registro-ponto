e194d2cbcb5c6b9a5f8ff06ad4410a8b6b2ebdef
Matheus Gomes <math.s.gomes@gmail.com>
2024-01-20 12:41:07 -0300

    alterar feriados

--- frontend_vue/src/views/FeriadosFormularioView.vue
+++ frontend_vue/src/views/FeriadosFormularioView.vue
@@ -1,8 +1,27 @@
 <template>
-  <h1 v-if="id" class="text-4xl font-extrabold">Alterar Feriado</h1>
+  <h1 v-if="id" class="text-4xl font-extrabold">Alterar Feriado ({{ id }})</h1>
   <h1 v-else class="text-4xl font-extrabold">Inserir Feriado</h1>
 
-  <p>Parâmetro da Rota: {{ id }}</p>
+  <div class="grid grid-flow-col gap-3 auto-cols-max">
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="data">
+        Data
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="data" type="date" placeholder="Data">
+    </div>
+
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="descricao">
+        Descrição
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="descricao" type="text" placeholder="Ex: Proclamação da República">
+    </div>
+  </div>
+
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Salvar
+  </button>
+
 </template>
 
 <script>

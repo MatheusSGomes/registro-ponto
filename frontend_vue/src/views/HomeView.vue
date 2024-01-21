7fdf4e6f9bd90e4da727c70529a7925f3a34ca7c
Matheus Gomes <math.s.gomes@gmail.com>
2024-01-20 23:51:16 -0300

    adicionado data e hora

--- frontend_vue/src/views/HomeView.vue
+++ frontend_vue/src/views/HomeView.vue
@@ -3,16 +3,19 @@
     <h1 class="text-4xl font-extrabold">Registro de Ponto</h1>
 
     <div class="">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="cpf">Digite sua matricula: </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="matricula" type="text" />
+      <label class="block text-gray-700 text-sm font-bold my-2" for="cpf">Digite sua matricula: </label>
+      <div class="flex gap-3">
+        <input v-model="colaborador.matricula" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="matricula" type="text" />
+        <button @click="buscaColaborador" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Confirmar</button>
+      </div>
     </div>
 
-    <h1 class="text-4xl font-extrabold my-5">20/01/2024</h1>
-    <h1 class="text-2xl font-extrabold">18:05</h1>
+    <h1 class="text-4xl font-extrabold my-5">{{ data_atual }}</h1>
+    <h1 class="text-2xl font-extrabold">{{ hora_atual }}</h1>
 
     <div class="flex justify-between my-10">
-      <p><strong>Colaborador: </strong>John Due</p>
-      <p><strong>Matricula: </strong>555</p>
+      <p><strong>Colaborador: </strong>{{ colaborador.nome }}</p>
+      <p><strong>Matricula: </strong>{{ colaborador.id }}</p>
     </div>
 
     <div>
@@ -43,14 +46,15 @@
 </template>
 
 <script>
+import ColaboradoresDataService from '@/services/ColaboradoresDataService'
 
 export default {
   name: 'HomeView',
   components: { },
   data() {
     return {
-      data_atual: null,
-      hora_atual: null,
+      data_atual: new Date().toLocaleDateString('pt-br'),
+      hora_atual: new Date().getHours() + ':' + new Date().getMinutes(),
       colaborador: {
         nome: null,
         matricula: null,
@@ -60,6 +64,18 @@ export default {
         { entrada: null, saida: null },
       ]
     }
+  },
+  methods: {
+    buscaColaborador() {
+      ColaboradoresDataService
+        .get(this.colaborador.matricula)
+        .then(response => {
+          this.colaborador = response.data;
+        });
+    }
+  },
+  mounted() {
+    console.log();
   }
 }
 </script>

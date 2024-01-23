0c6ad376183ac63e866b665abc17232d0400911c
Matheus Gomes <math.s.gomes@gmail.com>
2024-01-22 22:56:37 -0300

    novo layout colaboradores

--- frontend_vue/src/components/colaboradores/ColaboradoresTable.vue
+++ frontend_vue/src/components/colaboradores/ColaboradoresTable.vue
@@ -1,36 +1,34 @@
 <template>
-  <div class="">
-    <table class="table-auto">
-      <thead>
-        <tr>
-          <th class="px-4 py-2">#</th>
-          <th class="px-4 py-2">Usuário</th>
-          <th class="px-4 py-2">E-mail</th>
-          <th class="px-4 py-2">Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="colaborador in colaboradores" :key="colaborador.id">
-          <td class="border px-4 py-2">{{ colaborador.id }}</td>
-          <td class="border px-4 py-2">{{ colaborador.nome }}</td>
-          <td class="border px-4 py-2">{{ colaborador.email }}</td>
-          <td class="border px-4 py-2">
-            <div class="flex items-center justify-center">
-              <router-link :to="{ name: 'colaboradores.alterar', params: { id: colaborador.id } }">
-                <img alt="" class="w-12 block" src="@/assets/icons/edit.svg" />
-              </router-link>
-              <ModalExcluir
-                @btn-excluir="excluirColaborador(colaborador.id)"
-                modalTitle="Excluir colaborador"
-                :modalContent="'Tem certeza que deseja excluir o colaborador ' + colaborador.nome + ',' + colaborador.id + '?'">
-                <img alt="" class="w-6" src="@/assets/icons/delete.svg">
-              </ModalExcluir>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <table class="table-auto mx-auto my-10">
+    <thead>
+      <tr>
+        <th class="px-4 py-2">#</th>
+        <th class="px-4 py-2">Usuário</th>
+        <th class="px-4 py-2">E-mail</th>
+        <th class="px-4 py-2">Ações</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="colaborador in colaboradores" :key="colaborador.id">
+        <td class="border px-4 py-2">{{ colaborador.id }}</td>
+        <td class="border px-4 py-2">{{ colaborador.nome }}</td>
+        <td class="border px-4 py-2">{{ colaborador.email }}</td>
+        <td class="border px-4 py-2">
+          <div class="flex items-center justify-center">
+            <router-link :to="{ name: 'colaboradores.alterar', params: { id: colaborador.id } }">
+              <img alt="" class="w-12 block" src="@/assets/icons/edit.svg" />
+            </router-link>
+            <ModalExcluir
+              @btn-excluir="excluirColaborador(colaborador.id)"
+              modalTitle="Excluir colaborador"
+              :modalContent="'Tem certeza que deseja excluir o colaborador ' + colaborador.nome + ',' + colaborador.id + '?'">
+              <img alt="" class="w-6" src="@/assets/icons/delete.svg">
+            </ModalExcluir>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
--- frontend_vue/src/views/Colaboradores/ColaboradoresListaView.vue
+++ frontend_vue/src/views/Colaboradores/ColaboradoresListaView.vue
@@ -1,13 +1,17 @@
 <template>
-  <h1 class="my-4 text-4xl font-extrabold">Colaboradores</h1>
+  <section class="bg-gray-100 rounded-3xl my-7 py-8 px-4">
+      <div class="mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+        <h1 class="mb-6 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">Colaboradores</h1>
 
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <router-link :to="{ name:'colaboradores.inserir' }">
+            Inserir novo colaborador
+          </router-link>
+        </button>
 
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-    <router-link :to="{ name:'colaboradores.inserir' }">
-      Inserir novo colaborador
-    </router-link>
-  </button>
-  <ColaboradoresTable />
+        <ColaboradoresTable />
+      </div>
+  </section>
 </template>
 
 <script>

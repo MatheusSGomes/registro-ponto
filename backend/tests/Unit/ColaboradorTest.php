2398bba621fe883d1297a720b4436ba0aaa0f4c9
Matheus Gomes <math.s.gomes@gmail.com>
2024-01-27 12:30:55 -0300

    rascunho testes unitários colaborador

--- backend/tests/Unit/ColaboradorTest.php
+++ backend/tests/Unit/ColaboradorTest.php
@@ -3,12 +3,24 @@
 use App\Models\Colaborador;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-// Model: Colaborador
+beforeEach(function () {});
 
-//it('verifica se é possível criar um colaborador', function () {
-//    $colaborador = Colaborador::factory()->create();
-//
-//    expect($colaborador->id)->toBeInt();
-//    expect($colaborador->nome)->toBeString();
-//
-//})->uses(DatabaseTransactions::class);
+test('verifica se o cpf está sendo salvo sem máscara', function () {});
+
+test('verifica se o ativo é boolean', function () {});
+
+test('verifica se o nome é string', function () {});
+
+test('verifica se data_nascimento está no formato esperado (Y-m-d)', function () {});
+
+test('verifica se data_admissao está no formato esperado (Y-m-d)', function () {});
+
+test('verifica se cargo_id tem relacionamento com a tabela cargos', function () {});
+
+test('verifica se funcao_id tem relacionamento com a tabela funcoes', function () {});
+
+test('verifica se data_recisao está no formato esperado (Y-m-d)', function () {});
+
+test('verifica se usuario é cadastrado automaticamente ao criar um novo colaborador)', function () {});
+
+afterEach(function () {});
